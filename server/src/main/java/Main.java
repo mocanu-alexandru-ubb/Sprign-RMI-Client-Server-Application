3687c92c833220110059c60bc8566040500b9fa2
RazvanNazare <58268118+RazvanNazare@users.noreply.github.com>
2021-04-01 13:42:54 +0300

    Update Main.java

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -5,5 +5,5 @@ public class Main {
     public static void main(String[] args) {
         new AnnotationConfigApplicationContext(Config.class.getPackageName());
         System.out.println("Server started...");
-    }
+    } 
 }
